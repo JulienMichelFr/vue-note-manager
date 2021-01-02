6b39b56ad8af8a10715a9d20aa482058bbad2c67
Julien Michel <julien.michel@90tech.fr>
2021-01-02 15:39:30 +0100

    render markdown content

--- src/components/NoteEditor/NoteEditor.vue
+++ src/components/NoteEditor/NoteEditor.vue
@@ -6,10 +6,11 @@
         md-input(v-model="note.title", :required="true")#title
     div
         md-chips(v-model="note.tags", md-placeholder="Tags")#tags
-    div
+    .flex-container
       md-field
         label Content
         md-textarea(v-model="note.content", :required="true")
+      div(v-html="markdown")
     .md-layout.md-gutter.md-alignment-top-right
       md-button(type="submit").md-primary Save
 </template>
@@ -17,10 +18,21 @@
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { CreateNote } from "@/models";
+import MarkdownIt from "markdown-it";
+
+const Renderer = new MarkdownIt();
 
 @Component
 export default class NoteEditor extends Vue {
   @Prop() private note!: CreateNote;
+
+  get markdown(): string {
+    try {
+      return Renderer.render(this.note.content);
+    } catch (err) {
+      return err.message;
+    }
+  }
 
   submit() {
     this.$emit("submit", this.note);
@@ -28,4 +40,14 @@ export default class NoteEditor extends Vue {
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.flex-container {
+  display: flex;
+  flex-flow: row nowrap;
+  gap: 10px;
+
+  & > div {
+    width: 50%;
+  }
+}
+</style>
